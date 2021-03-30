cfe3b77402733524a87fb82aeea1b912cf817e28
naywilkins512 <nathanwilkins01@gmail.com>
2021-03-30 00:35:45 -0500

    pinned first panel

--- script.js
+++ script.js
@@ -2,13 +2,13 @@
 gsap.registerPlugin(ScrollTrigger);
 
 let tl = gsap.timeline({ defaults: { opacity: 0, ease: "back" } });
-let tl2 = gsap.timeline({  
+let tl2 = gsap.timeline({
     scrollTrigger: {
-    trigger: ".hi",
-    start: "top top",
-    scrub: 3
-    
-}
+        trigger: ".hi",
+        start: "top top",
+        scrub: 2
+
+    }
 
 
 });
@@ -39,26 +39,26 @@ function init() {
         .from(".hi", { ease: "linear", duration: 1 })
         .from(".header", { y: -150, duration: 2.5 }, "-=.4")
         .from(".bio", { y: 40, duration: 1, ease: "power4" }, "-=1")
-        .from(".scroll", {duration: 2}, "+=0.2")
+        .from(".scroll", { duration: 2 }, "+=0.2")
 
-        // to be scrolltriggered
+    // to be scrolltriggered
 
-        // .from("#project-header", { y: 100, opacity: 0})
-        // .to(".hi", { y: -100, opacity: 0, duration: 1 })
-        // .to(".header", { y: -250, opacity: 0.5, duration: 4 }, "<.3")
-        // .to(".bio", { y: -300, opacity: 0, duration: 4 }, "<.2")
-        // .from(".navbar", { opacity: 0, duration: 4 }, "<.2")
+    // .from("#project-header", { y: 100, opacity: 0})
+    // .to(".hi", { y: -100, opacity: 0, duration: 1 })
+    // .to(".header", { y: -250, opacity: 0.5, duration: 4 }, "<.3")
+    // .to(".bio", { y: -300, opacity: 0, duration: 4 }, "<.2")
+    // .from(".navbar", { opacity: 0, duration: 4 }, "<.2")
 
     //end timeline
 
     // to be scrolltriggered
 
-   
 
-    tl2.to(".hi", {  opacity: 0 })
-        .to(".header", {  opacity: 0, duration: 4 }, "<.3")
+
+    tl2.to(".hi", { opacity: 0 })
+        .to(".header", { duration: 4 }, "<.3")
         .to(".bio", { opacity: 0, duration: 4 }, "<.2")
-        .from(".navbar", { opacity: 0, duration: 4 }, "<1")
+    // .from(".navbar", { opacity: 0, duration: 4 }, "<1")
 
 
 
@@ -67,7 +67,7 @@ function init() {
             trigger: "#projects > .col-sm",
             start: "top 80%",
             scrub: 2,
-            markers:true
+
         },
         y: 100, opacity: 0, ease: "back", stagger: .1, duration: 2
     });
@@ -76,13 +76,36 @@ function init() {
         scrollTrigger: {
             trigger: ".project-header",
             start: "top 60%",
-            scrub:true
+            scrub: true,
         },
-         y: 100, opacity: 0
+        y: 100, opacity: 0
     });
 
 
-      
+    gsap.from(".panel-2", {
+        scrollTrigger: {
+            trigger: ".panel-1",
+            start: "top top",
+            end: "100%",
+            scrub:true,
+            pin:true,
+            anticipatePin:1,
+        },
+        yPercent: 100
+    })
+
+    // gsap.from(".panel-3", {
+    //     scrollTrigger: {
+    //         trigger: ".panel-2",
+    //         start: "top top",
+    //         end:"bottom",
+    //         scrub:true,
+    //         pin:true,
+    //         anticipatePin:1,
+    //         markers: true
+    //     },
+    //     xPercent: 100
+    // })
 
 }
 
